ab06804d255989386dbe3a85c3d8fffc3df8bb4b
nahuew <nahuewrodriguez@gmail.com>
2026-04-22 22:31:29 -0300

    EStructura de carpetas, index.js y package hechos

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json()); // Recibe el JSON en el cuerpo de las peticiones
+app.use(express.urlencoded({ extended: true })); // Permite recibir datos de formularios
+
+
+// --- Iniciar servidor ---
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto http://localhost: ${PORT}`);
+});
+
+
+// --- MIDDLEWARES ---
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'pug');
+
+// --- Ruta Archivos estáticos ---
+app.use(express.static(path.join(__dirname, 'public')));
+
+// --- Ruta De Pruebas ---
+app.get('/', (req, res) => {
+    // esta vacio porque como hay que hacerlo manual, por lo que dijo el profe,
+    // depende de que tengamos las clases creadas
+});
+
+// --- CLASES ---
+
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+La carpeta de node_modules no la subí porque pesa mucho tienen que instarl ustedes node y express por su cuenta 
+
+#AVISO
+
+Antes de instalar express tiren el comando npm express -v, si les aparece la versión es porque ya esta integrado
+(dudo que quede completo así que por las dudas instalenlo pero es para precaución)
