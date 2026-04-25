fa6c26afc80e539c135e0e6bf32ea66711388b61
gildamorgante <gigimorgante90@gmail.com>
2026-04-25 08:31:25 -0300

    Corrección en budget

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const Job = require("../models/Job");
 const jobsPath = path.join(__dirname, "../data/job.json");
-const budgetsPath = path.join(__dirname, "../data/budget.json");
 
 // Reads data from the JSON file and returns it as a JavaScript object
 const jobsDataReader = () => {
@@ -127,47 +126,6 @@ const deleteJob = (req, res) => {
 
 };
 
-// Read budgets
-const readBudgets = () => {
-    try {
-        const data = fs.readFileSync(budgetsPath, "utf-8");
-        return JSON.parse(data || "[]");
-    } catch (error) {
-        console.error("Error leyendo budgets:", error);
-        return [];
-    }
-};
-
-// GET budgets by job_id
-const getBudgetsByJob = (req, res) => {
-    const jobId = parseInt(req.params.jobId);
-    console.log("Buscando presupuestos para obra ID:", jobId);
-    const budgets = readBudgets();
-
-    const jobBudgets = budgets.filter(b => b.job_id === jobId);
-
-    if (jobBudgets.length === 0) {
-        return res.status(404).json({
-            message: "No hay presupuestos para esta obra"
-        });
-    }
-
-    res.json(jobBudgets);
-};
-
-// GET VIEW budgets by job_id
-const getBudgetsView = (req, res) => {
-    const jobId = parseInt(req.params.jobId);
-
-    const budgets = readBudgets();
-
-    const jobBudgets = budgets.filter(b => b.job_id === jobId);
-
-    res.render("budgets", { 
-        budgets: jobBudgets,
-        jobId
-    });
-};
 
 module.exports = {
     getJobs,
@@ -178,6 +136,4 @@ module.exports = {
     deleteJob,
     newJobForm,
     getJobDetailView,
-    getBudgetsByJob,
-    getBudgetsView
 };
